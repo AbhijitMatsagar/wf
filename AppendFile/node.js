dcfa21cf86a45396ea54630f20b08e483a310716
Abhijit <abhibmatsagar@gmail.com>
2022-12-30 19:25:35 +0530

    Second Commit

--- AppendFile/node.js
+++ AppendFile/node.js
@@ -1,23 +1,8 @@
-// Node.js program to demonstrate the
-// fs.appendFile() method
 
-// Import the filesystem module
 const fs = require('fs');
 
-// Get the file contents before the append operation
-console.log("\nFile Contents of first file before append:",
-fs.readFileSync("file1.txt", "utf8"));
-
-console.log("\nFile Contents of second file before append:",
-fs.readFileSync("file2.txt", "utf8"));
-
-fs.appendFile("file2.txt", "file1.txt", (err) => {
+fs.appendFile("file1.txt", "I am __________", (err) => {
 if (err) {
 	console.log(err);
-}
-else {
-	// Get the file contents after the append operation
-	console.log("\nFile Contents of file after append:",
-	fs.readFileSync("file2.txt", "utf8"));
 }
 });
